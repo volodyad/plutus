33a00315d57cf4f418363f4049574a1e05262442
Moritz Angermann <moritz.angermann@gmail.com>
2021-06-10 07:43:27 +0000

    Used stdenv to detect isGhcjs, not crossSystem.

--- nix/default.nix
+++ nix/default.nix
@@ -38,7 +38,7 @@ let
     config = haskellNix.config // config;
   };
 
-  ghcjsPluginPkgs = if system == builtins.currentSystem && !crossSystem.isGhcjs
+  ghcjsPluginPkgs = if system == builtins.currentSystem && !pkgs.stdenv.isGhcjs
                     then null
                     else import ./pkgs {
       pkgs = nativePkgs;
